a59ee331671dd91d5439822f9ae059303cc1fa09
VFedorch <Valentynlisicyn@gmail.com>
2021-03-12 02:16:55 +0200

    changed page layout & added new indicators

--- OWM_page.py
+++ OWM_page.py
@@ -1,15 +1,19 @@
 import json
 import time
 import requests
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    weather = parse_request()
-    return render_template('index.html', item=weather)
+    Visitor_ip = get_visitor_ip()
+    print("\nVisitor's IP: " + Visitor_ip + "\n")
+    return render_template('index.html', item=parse_request())
+
+@app.route("/", methods=["GET"])
+def get_visitor_ip():
+    return request.remote_addr
 
 def get_location():
     get_location_url = 'http://ipinfo.io/json'
@@ -28,21 +32,44 @@ def parse_request():
     location = get_location()
     city = location[0]
     country = location[1]
+    Weatherinfo = {"Location": "Unknown", "Description": "Unknown", "Icon": "Unknown", "Background": "Unknown","Temperature": "Unknown", "Humidity": "Unknown", "Wind_speed": "Unknown", "Pressure": "Unknown" }
+    dirs = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
     weather_main_url = "http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&units="+units+"&appid="+api_key 
 
     response = requests.get(weather_main_url)
     json_response = response.json()
     
-    Weatherinfo = dict()
-    Weatherinfo['Location'] = "Showing Weather for: " + city
-    Weatherinfo['Description'] = "Current atmospheric description: " + json_response["weather"][0]["description"]
-    Visual_description = json_response["weather"][0]["icon"]
-    Weatherinfo['Icon'] = "static/images/" + Visual_description + "_icon.png"
-    Weatherinfo['Background'] = "static/images/" + Visual_description + "_art.jpg"
-    Weatherinfo['Temperature'] = "Current Temperature:" + str(round(json_response["main"]["temp"])) + "˚C"
-    Weatherinfo['Humidity'] = "Humidity:" + str(json_response["main"]["humidity"]) + "%"
-    Weatherinfo['Wind_speed'] = "Wind speed:" + str(round(json_response["wind"]["speed"])) + "mph"
-    Weatherinfo['Pressure'] = "Atmospheric Pressure:" + str(json_response["main"]["pressure"]) + "mmRs"
+    if json_response["cod"] == 400:
+        print("\n Nothing to geocode \n")
+
+    elif json_response["cod"] == 401:
+        print("\n Invalid API key. Please see http://openweathermap.org/faq#error401 for more info. \n")
+        
+    elif json_response["cod"] == 404:
+        print("\n City not found. Please see http://openweathermap.org/faq#error404 for more info. \n")
+        
+    elif json_response["cod"] == 429:
+        print("\n You have free tariff and make more than 60 API calls per minute. Please see http://openweathermap.org/faq#error429 for more info. \n")
+        
+    elif 'weather' not in json_response or 'main' not in json_response or 'wind' not in json_response:
+        print(json_response)
+        print("\n \"weather\" or \"main\" or \"wind\" not in response \n")
+        
+    else:
+        Weatherinfo["Location"] = city
+        Weatherinfo["Description"] = json_response["weather"][0]["description"]
+        Visual_description = json_response["weather"][0]["icon"]
+        Weatherinfo["Icon"] = "static/images/" + Visual_description + ".png"
+        Weatherinfo["Background"] = "/static/images/" + Visual_description + "_art.jpg"
+        Weatherinfo["Temperature"] = str(round(json_response["main"]["temp"])) + "˚C"
+        Weatherinfo["Humidity"] = str(json_response["main"]["humidity"]) + "%"
+        Weatherinfo["Wind_speed"] = str(round(json_response["wind"]["speed"])) + " m/s"
+        Weatherinfo["Pressure"] = str(json_response["main"]["pressure"]) + " mb"
+        Weatherinfo["Feels_like"] = str(json_response["main"]["feels_like"]) + "˚C"
+        Visibility_km = int(json_response["visibility"]) / 1000
+        Weatherinfo["Visibility"] = str(Visibility_km) + " km"
+        ix = round(json_response["wind"]["deg"] / (360. / len(dirs)))
+        Weatherinfo["Wind_direction"] = dirs[ix % len(dirs)]
 
     return Weatherinfo
 
