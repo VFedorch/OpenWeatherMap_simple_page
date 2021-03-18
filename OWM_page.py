3b9bc98f3572dc12dfdd7fcc38e50955e06888ec
VFedorch <Valentynlisicyn@gmail.com>
2021-03-18 23:39:54 +0200

    Added home page, edited tiles, and others

--- OWM_page.py
+++ OWM_page.py
@@ -5,16 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Visitor_ip = get_visitor_ip()
-    # print("\nVisitor's IP: " + Visitor_ip + "\n")
-    weather_info = weather()
-    curent_weather = weather_info[0]
-    seven_days_forecast = weather_info[1]
-    return render_template('index.html', item=curent_weather, forecast_items=seven_days_forecast)
+    return render_template('home.html')
 
 @app.route("/", methods=["GET"])
 def get_visitor_ip():
     return request.remote_addr
+
+@app.route("/weather_display")
+def weather_display():
+    print("\nVisitor's IP: " + get_visitor_ip() + "\n")
+    weather_info = weather()
+    curent_weather = weather_info[0]
+    seven_days_forecast = weather_info[1]
+    return render_template('weather_display.html', item=curent_weather, forecast_items=seven_days_forecast)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
