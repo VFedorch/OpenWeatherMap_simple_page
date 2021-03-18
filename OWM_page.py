4d9259b8ff1e8d7afb07e78a9be744692d1d49c6
VFedorch <Valentynlisicyn@gmail.com>
2021-03-19 01:38:02 +0200

    Added select location page

--- OWM_page.py
+++ OWM_page.py
@@ -5,19 +5,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('home.html', )
 
 @app.route("/", methods=["GET"])
 def get_visitor_ip():
     return request.remote_addr
 
-@app.route("/weather_display")
+@app.route("/weather_display", methods = ['POST', 'GET'])
 def weather_display():
     print("\nVisitor's IP: " + get_visitor_ip() + "\n")
+    location = request.form.get('location_form')
+    print(location)
     weather_info = weather()
     curent_weather = weather_info[0]
     seven_days_forecast = weather_info[1]
-    return render_template('weather_display.html', item=curent_weather, forecast_items=seven_days_forecast)
+    return render_template('weather_display.html', item=curent_weather, forecast_items=seven_days_forecast,)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
