4deae1bd3eec83fd83b097e495327005aae38e1f
VFedorch <Valentynlisicyn@gmail.com>
2021-03-20 03:05:19 +0200

    Grafics for home page, changed forecast drawing method

--- OWM_page.py
+++ OWM_page.py
@@ -19,7 +19,7 @@ def weather_display():
     weather_info = weather()
     curent_weather = weather_info[0]
     seven_days_forecast = weather_info[1]
-    return render_template('weather_display.html', item=curent_weather, forecast_items=seven_days_forecast,)
+    return render_template('weather_display.html', item=curent_weather, forecast_items=seven_days_forecast)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
